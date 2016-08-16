6da6e30ae8825d621bf1678c55ea1314c10dd14d
Peter <peter.john.henggeler@gmail.com>
2016-08-16 15:30:59 -0600

    web_scraped is commented out

--- app/models/web_scraper.rb
+++ app/models/web_scraper.rb
@@ -11,37 +11,37 @@ require 'net/http'
 
 #this is how we request the page we are going to scrape
 
- 
-         uri = URI('http://'+'markmonitor.com')
 
-        begin
-          page = HTTParty.get(uri)
-        rescue SocketError => e
-          #redirect_to new_domain_path, flash: {notice: "Cannot validate domain"} and return 
-        end
-        doc = Nokogiri::HTML(open(uri,:allow_redirections => :all))
-        #this is where we transfer our http response into Nokogiri object
-        parse_page = Nokogiri::HTML(page)
-        doc = Nokogiri::HTML(open(uri,:allow_redirections => :all))
+#         uri = URI('http://'+'markmonitor.com')
+#
+#        begin
+#          page = HTTParty.get(uri)
+#        rescue SocketError => e
+#          #redirect_to new_domain_path, flash: {notice: "Cannot validate domain"} and return
+#        end
+#        doc = Nokogiri::HTML(open(uri,:allow_redirections => :all))
+#        #this is where we transfer our http response into Nokogiri object
+#        parse_page = Nokogiri::HTML(page)
+#        doc = Nokogiri::HTML(open(uri,:allow_redirections => :all))
 
         #puts parse_page.css('title').text
         #puts parse_page.css('h2')
         #puts parse_page.css('body').text
-        parse_page.xpath("//text()").text
-        parse_page.css('h2').each do |h2|
+#        parse_page.xpath("//text()").text
+#        parse_page.css('h2').each do |h2|
             #puts h2.text
-        end
-        
-        parse_page.css('h3').each do |h3|
+#        end
+
+#        parse_page.css('h3').each do |h3|
             #puts h3.text
-        end
-        puts parse_page.css('p').text
-        parse_page.css("a").text
+#        end
+#        puts parse_page.css('p').text
+#        parse_page.css("a").text
         #puts links.length
-        links.each do |link|
-          if (!link['href'].to_s.include? 'markmonitor.com')
-            #puts link['href']
-          end
-        end
+#        links.each do |link|
+#          if (!link['href'].to_s.include? 'markmonitor.com')
+#            #puts link['href']
+#          end
+#        end
         #puts doc.xpath('//meta[@name="Description"]/@content').text
-        #puts doc.xpath('//meta[@name="Keywords"]/@content').textrails 
+        #puts doc.xpath('//meta[@name="Keywords"]/@content').textrails
