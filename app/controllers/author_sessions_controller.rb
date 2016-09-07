60fb2dd462a342fc1b42825ff27ac7834a72d3d1
Peter Henggeler <peter.john.henggeler@gmail.com>
2016-09-07 03:39:29 +0000

    try this

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -4,10 +4,10 @@ class AuthorSessionsController < ApplicationController
   end
 
   def create
-#    if (!Author.verified?(params[:email]))
-#        flash[:alert] = 'You are Not Verified Yet'
-#        redirect_to(new_author_session_path)
-#    else
+    if (!Author.verified?(params[:email]))
+        flash[:alert] = 'You are Not Verified Yet'
+        redirect_to(new_author_session_path)
+    else
       if login(params[:email], params[:password])
         flash[:notice] = 'Logged In Successfully'
         redirect_back_or_to(authors_path)
@@ -22,5 +22,5 @@ class AuthorSessionsController < ApplicationController
     logout
     redirect_to(new_author_session_path, notice: 'Logged out!')
   end
-  
+end
 end
