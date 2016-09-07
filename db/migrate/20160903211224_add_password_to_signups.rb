254a1ead7654d504e99b8947fff6393fc7e5fa0b
Peter Henggeler <peter.john.henggeler@gmail.com>
2016-09-07 03:22:38 +0000

    changes to migration file

--- db/migrate/20160903211224_add_password_to_signups.rb
+++ db/migrate/20160903211224_add_password_to_signups.rb
@@ -1,6 +1,6 @@
 class AddPasswordToSignups < ActiveRecord::Migration[5.0]
   def change
-    add_column :signups, :password, :string
-    add_column :signups, :password_confirmation, :string
+    #add_column :signups, :password, :string
+    #add_column :signups, :password_confirmation, :string
   end
 end
