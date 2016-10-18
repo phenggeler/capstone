d8e318014bac4af0d8bfb86b79443ae19f48f057
Peter Henggeler <peter.john.henggeler@gmail.com>
2016-10-18 19:15:39 +0000

    made change to env file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "work_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  DISABLE_DATABASE_ENVIRONMENT_CHECK=1
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
