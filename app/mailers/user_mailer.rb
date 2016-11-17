e5bf53633b84a875a1db1e6264ded0978733bfc4
Peter Henggeler <peter.john.henggeler@gmail.com>
2016-11-17 19:57:18 +0000

    changed user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
 
     def welcome_email(user)
-        @user = author
+        @user = user
         mail(to: @user.email, subject: 'Welcome Email')
     end
     
